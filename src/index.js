9ec6696a9e02b290df2f9d40e23345765a138e14
Yahor Harai <egor.garai@gmail.com>
2022-08-13 01:32:05 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -37,10 +37,45 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
-}
-
-module.exports = {
-    decode
-}
+function decodeNumberInMorse(expr) {
+    let decode = [];
+  
+    for (let i = 0; i < expr.length; i += 10) {
+      const strTen = expr.substr(i, 10);
+      let firstStr = '';
+  
+      for (let t = 0; t < strTen.length; t += 2) {
+        const secondStr = strTen.substr(t, 2);
+  
+        switch (secondStr) {
+          case "10":
+              firstStr = firstStr + '.';
+            break;
+          case "11":
+              firstStr = firstStr + '-';
+            break;
+          case "**":
+              firstStr = '*';
+            break;
+        }
+      }
+      
+      decode.push(firstStr);
+    }
+    return decode;
+  }
+  
+  function decodeMorseInReadable(morse)
+  {
+      return morse.map(item => item === "*" ? " " : MORSE_TABLE[item]).join("");
+  }
+  
+  function decode(expr) {
+    const getMorseText = decodeNumberInMorse(expr);
+    const resultText = decodeMorseInReadable(getMorseText);
+    return resultText;
+  }
+    
+  module.exports = {
+    decode,
+  };
